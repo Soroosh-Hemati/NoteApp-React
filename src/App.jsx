e16714ae73b70ff46e24c53c70ede9b54dfd6ba7
Soroosh-Hemati <sorooshhemati4@gmail.com>
2024-07-22 12:44:50 +0330

    AddNewNote component completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import AddNewNote from './components/AddNewNote'
+import NoteList from './components/NoteList'
 import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [notes,setNotes] = useState([]);
+  const handleAddNote = (newNote)=>{
+    setNotes((prevNotes)=>[...prevNotes,newNote])
+  }
+
   return <div className='container'>
     <div className="note-header">note header</div>
     <div className="note-app">
-      <AddNewNote></AddNewNote>
-      <div className="note-container">note container</div>
+      <AddNewNote onAddNote={handleAddNote}/>
+      <div className="note-container">
+        <NoteList notes={notes}/>
+        </div>
     </div>
   </div>
 
--- src/components/AddNewNote.jsx
+++ src/components/AddNewNote.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import { useState } from "react";
 
-function AddNewNote() {
-  return (
-    <div className="add-new-note">
-        <h2>Add New Note</h2>
-        <form action="" className='note-form'>
-            <input type="text" placeholder='Note Title...' />
-            <input type="text" placeholder='Note Description...' className='note-desc-input'/>
-            <button className='btn--primary'>Add New Note</button>
-        </form>
-      </div>
-  )
+
+function AddNewNote({onAddNote}) {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(!title || !description) return null
+        const newNote = {
+            title,
+            description,
+            id: Date.now(),
+            Completed: false,
+            createdAt: new Date().toISOString()
+        }
+        onAddNote(newNote)
+        setTitle('')
+        setDescription('')
+    }
+    
+    return (
+        <div className="add-new-note">
+            <h2>Add New Note</h2>
+            <form className='note-form' onSubmit={handleSubmit}>
+                <input type="text" placeholder='Note Title...' value={title} onChange={(e) => setTitle(e.target.value)} />
+                <input type="text" placeholder='Note Description...' value={description} className='note-desc-input' onChange={(e) => setDescription(e.target.value)} />
+                <button className='btn btn--primary'>Add New Note</button>
+            </form>
+        </div>
+    )
 }
 
 export default AddNewNote
